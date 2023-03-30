ea5e8ac2d1aa867145baa250691993519eeec1b4
Tin Napret <tin.napret@student.um.si>
2023-03-30 03:57:15 +0200

    Narejena funkcija, ki: - pridobi ime datoteke iz podane poti do datoteke - prebere vsebino datoteke ter shrani vsebino v polje A - narejen glavni program

--- VerzioniranjeDN1/VerzioniranjeDN1/Program.cs
+++ VerzioniranjeDN1/VerzioniranjeDN1/Program.cs
@@ -1,12 +1,98 @@
 ﻿using System;
+using System.IO;
 
 namespace VerzioniranjeDN1
 {
+    
     class Program
     {
+        // PREVERI POT DATOTEKE
+        public static string pridobiImeDatoteke(string potDoDatoteke)
+        {
+            // Preverimo ali obstaja datoteka v dani poti
+            if (!File.Exists(potDoDatoteke))
+            {
+                return "";
+            }
+
+            return Path.GetFileName(potDoDatoteke);
+        }
+
+        // PREBERI DATOTEKO IN ZAPISI V POLJE
+        public static int[] preberiDatoteko(string imeDatoteke)
+        {
+            char delilniZnak = ' ';
+            int velikostPoljaA = 0;
+            int indexPoljaA = 0;
+
+            // PRESTEJEMO ST. ZAPISOV STEVIL V DATOTEKI
+            try
+            {
+                using (StreamReader sr = new StreamReader(imeDatoteke))
+                {
+                    string vrstica;
+
+                    while ((vrstica = sr.ReadLine()) != null)
+                    {
+                        string[] stevilaVrstice = vrstica.Split(delilniZnak);
+
+                        velikostPoljaA += stevilaVrstice.Length;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Napaka pri branju iz datoteke: " + e.Message);
+            }
+
+            // kreiramo polje A velikosti ki smo jo prebrali iz datoteke
+            int[] A = new int[velikostPoljaA];
+
+            // NAPOLNIMO POLJE A
+            try
+            {
+                using (StreamReader sr = new StreamReader(imeDatoteke))
+                {
+                    string vrstica;
+
+                    while ((vrstica = sr.ReadLine()) != null)
+                    {
+                        // razdelimo vrstico datoteke po delimetru: ' '
+                        string[] stevilaVrstice = vrstica.Split(delilniZnak);
+
+                        foreach (string stevilo in stevilaVrstice)
+                        {
+                            A[indexPoljaA] = int.Parse(stevilo);
+                            indexPoljaA++;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Napaka pri branju iz datoteke: " + e.Message);
+            }
+
+            return A;
+        }
+
+        // GLAVNI PROGRAM
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string potDoDatoteke = args[0];
+            string imeDatoteke = pridobiImeDatoteke(potDoDatoteke);
+
+            if (imeDatoteke != "")
+            {
+                // OK
+                int[] A = preberiDatoteko(imeDatoteke);
+
+            }
+            else
+            {
+                // ERR
+                Console.WriteLine("Dana datoteka ne obstaja - preverite zapis poti");
+            }
         }
     }
 }
